710fd7d01031d2e52c823c2f0ba5d27fe80ce43b
abt434 <abt434@141.22.40.143>
2016-04-15 14:37:04 +0200

    Yellowpage Service registration & dice

--- VSPRestopolyA/src/main/java/org/haw/vsp/restopoly/StartUp.java
+++ VSPRestopolyA/src/main/java/org/haw/vsp/restopoly/StartUp.java
@@ -9,47 +9,38 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import static spark.Spark.*;
 
 public class StartUp {
-	
+
 	private static final String YELLOW_PAGES = "http://172.18.0.5:4567/services";
-	
+
+	private static final String SERVICE_URI = "http://abl459-services:4567/";
+
 	public static void main(String[] args) {
 		get("/hello", (request, response) -> "Hello World");
 		get("/dice", Dice::roll);
-		//Dice service registration with Yellow Pages
-		try {
-			JSONObject json = new JSONObject();
-			json.put("name", "DiceService");
-			json.put("description", "A service for rolling a dice");
-			json.put("service", "dice");
-			json.put("uri", "http://abl459_docker_0:4567/dice");
-			String result = Unirest.put(YELLOW_PAGES + "/1")
-				.header("Content-Type", "application/json")
-				.body(json)
-				.asString().getBody();
-			System.out.println("Yellow-Pages Registration wurde versendet!");
-			System.out.println("Versendetes JSON: " + json.toString());
-			System.out.println("Result: " + result);
-		} catch (UnirestException e) {
-			e.printStackTrace();
-		}
-		
+
+		registerService("DiceService", "A service for rolling a dice", "dice", "dice", 4);
+
 		get("/users", Users::getUsers);
 		post("/users", Users::postUser);
 		get("/users/:id", Users::getUser);
 		delete("/users/:id", Users::deleteUser);
 		put("/users/:id", Users::putUser);
-		//Users service registration with Yellow Pages
+
+		registerService("UsersService", "A service for managing users", "users", "users", 2);
+
+	}
+
+	private static void registerService(String name, String description, String service, String uri, int id) {
 		try {
 			JSONObject json = new JSONObject();
-			json.put("name", "UsersService");
-			json.put("description", "A service for managing users");
-			json.put("service", "users");
-			json.put("uri", "http://abl459_docker_0:4567/users");
-			String result = Unirest.put(YELLOW_PAGES + "/2")
-				.header("Content-Type", "application/json")
-				.body(json)
-				.asString().getBody();
-			System.out.println("Yellow-Pages Registration wurde versendet!");
+			json.put("name", name);
+			json.put("description", description);
+			json.put("service", service);
+			json.put("uri", SERVICE_URI + uri);
+			String yellowURI = YELLOW_PAGES + "/" +Integer.toString(id);
+			String result = Unirest.put(yellowURI).header("Content-Type", "application/json")
+					.body(json).asString().getBody();
+			System.out.println("Yellow-Pages Registration wurde versendet, an : " + yellowURI + "!");
 			System.out.println("Versendetes JSON: " + json.toString());
 			System.out.println("Result: " + result);
 		} catch (UnirestException e) {
--- VSPRestopolyA/src/main/java/org/haw/vsp/restopoly/services/Dice.java
+++ VSPRestopolyA/src/main/java/org/haw/vsp/restopoly/services/Dice.java
@@ -20,7 +20,7 @@ public class Dice {
 		String game = request.queryParams("game");
 		int result = (Integer) ThreadLocalRandom.current().nextInt(1, 7);
 		try {
-			HttpResponse<String> jsonResponse = Unirest.post("http://abq335_events:4567/events")
+			HttpResponse<String> jsonResponse = Unirest.post("http://abs969-events:4567/events")
 					.queryString("game", game)
 					.queryString("type", "Dice Event")
 					.queryString("name", "Dice Event")
