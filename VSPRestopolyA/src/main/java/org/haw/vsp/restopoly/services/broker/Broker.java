e06853834f758d6ba97f0ecf72fe807578aefdaf
abt434 <abt434@141.22.40.188>
2016-05-19 08:46:28 +0200

    .

--- VSPRestopolyA/src/main/java/org/haw/vsp/restopoly/services/broker/Broker.java
+++ VSPRestopolyA/src/main/java/org/haw/vsp/restopoly/services/broker/Broker.java
@@ -1,7 +1,75 @@
+
 package org.haw.vsp.restopoly.services.broker;
 
 import org.haw.vsp.restopoly.services.Service;
 
-public class Broker extends Service{ //TODO implement the Broker
+import spark.Request;
+import spark.Response;
 
+public class Broker extends Service{
+	
+	protected static final String NAME = "BrokerService";
+
+	public static final String DESCRIPTION = "A service for managing estates";
+
+	public static final String SERVICE_NAME = "broker";
+
+	public static final String SERVICE_URI = "/broker";
+
+	public static String getBrokers(Request request, Response response) {
+		return "";
+	}
+	
+	public static String postBroker(Request request, Response response) {
+		return "";
+	}
+	
+	public static String getBrokerByGameId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String putBrokerByGameId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String getPlacesByGameId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String getPlaceByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String putPlaceByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String getOwnerByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String putOwnerByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String postOwnerByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String putHypoByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String deleteHypoByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String postVisitByPlaceId(Request request, Response response) {
+		return "";
+	}
+	
+	public static String getName() {
+		return NAME + GROUP_NAME;
+	}
 }
+
